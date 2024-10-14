a3f0db74d62216d9f05eb9ce364946dfcfa16f3b
Karol Kopicera <karol.kopicera@softwarehut.com>
2024-10-14 19:33:51 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import InputComponent from './components/InputComponent';
+import ButtonComponent from './components/ButtonComponent';
+import TextComponent from './components/TextComponent';
+import { AppProvider } from './context/context';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider>
+      <div className="App">
+        <header className="App-header">
+          <div className="top-section">
+            <InputComponent />
+            <ButtonComponent />
+          </div>
+          <TextComponent />
+        </header>
+      </div>
+    </AppProvider>
   );
 }
 
---
+++ src/components/TextComponent.js
@@ -0,0 +1,22 @@
+import React, { useContext, useRef, useEffect } from 'react';
+import { InputValueContext, ClickCountContext } from '../context/context';
+
+const TextComponent = () => {
+  const { inputValue } = useContext(InputValueContext);
+  const { clickCount } = useContext(ClickCountContext);
+  const renderCount = useRef(0);
+
+  useEffect(() => {
+    renderCount.current = renderCount.current + 1;
+  });
+
+  return (
+    <div style={{ textAlign: 'center', border: '1px solid black', padding: '10px' }}>
+      <p>Input value: {inputValue}</p>
+      <p>Button click count: {clickCount}</p>
+      <p>Render count: {renderCount.current}</p>
+    </div>
+  );
+}
+
+export default React.memo(TextComponent);
